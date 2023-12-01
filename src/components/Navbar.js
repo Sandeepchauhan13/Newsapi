2596da556ee9dc759da45db73db80ef3b43db8a1
Sandeepchauhan13 <147958447+Sandeepchauhan13@users.noreply.github.com>
2023-12-01 14:43:33 +0530

    completed entire application

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 // import React, { Component } from 'react'
 import React from 'react'
 import {Link} from 'react-router-dom';
-import PropTypes from 'prop-types';
+
 import moment from 'moment';
 // export class Navbar extends Component {
   const Navbar = (props)=>{
@@ -9,12 +9,13 @@ import moment from 'moment';
     return (
       <div>
            <nav className="navbar fixed-top  navbar-expand-lg bg-dark">
+           {/* <nav className="navbar fixed-top  navbar-expand-dark bg-white"> */}
   <div className="container-fluid">
     <Link className="navbar-brand newsapp" to="/">News-WebApp</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+      <span className="navbar-toggler-icon span"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <div className="collapse navbar-collapse collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item"><Link className="nav-link" aria-current="page" to="/general">Home</Link></li>
         {/* <li className="nav-item"><Link className="nav-link" href="/">About</Link></li> */}
